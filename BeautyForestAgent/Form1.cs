6ac554db26a128a4349c6873c517886d0cd1baa8
Jiyeon Choi <ccy5906@naver.com>
2019-06-20 11:40:29 +0900

    #3 트레이아이콘 코드 구현 by 3517 최지연

--- BeautyForestAgent/Form1.cs
+++ BeautyForestAgent/Form1.cs
@@ -16,6 +16,37 @@ namespace BeautyForestAgent
             InitializeComponent();
         }
 
-  
+        private void VisibleChange(bool visi)
+        {
+            this.Visible = visi;  //창
+            this.notiTray.Visible = !visi;  //트레이 아이콘
+        }
+
+        private void BtnTray_Click(object sender, EventArgs e)
+        {
+            VisibleChange(false);  //버튼 클릭 시 창 숨기기
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;  //X버튼 클릭시 종료되는 것을 취소함
+            VisibleChange(false);
+        }
+
+        private void 폼보이기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            VisibleChange(true);
+        }
+
+        private void 종료ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.notiTray.Visible = false;
+            Application.ExitThread(); //모두 종료
+        }
+
+        private void NotiTray_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            VisibleChange(true);
+        }
     }
 }
